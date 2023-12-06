0c9abe51799efcc4113b238ba630fdab9360396a
SaheedGitIvyTech <143648082+SaheedGitIvyTech@users.noreply.github.com>
2023-12-05 19:24:23 -0500

    SaheedM06

--- app.js
+++ app.js
@@ -1,132 +1,45 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogRoutes = require('./routes/blogRoutes');
 
-
-//express app - used instead of server.js
+// express app
 const app = express();
 
-//connect to mongodb
-const dbURI = 'mongodb+srv://netninja:Zaaffy2010@cluster0.fdxdlrw.mongodb.net/Cluster0?retryWrites=true&w=majority';
-mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-    //.then((result) => console.log('connected to db'))
-    .then((result) => app.listen(3000))
-    .catch((err) => console.log(err))
+// connect to mongodb & listen for requests
+//const dbURI = "mongodb+srv://netninja:test_1234@net-ninja-tuts-del96.mongodb.net/node-tuts";
+const dbURI = "mongodb+srv://netninja:test_123@cluster0.fdxdlrw.mongodb.net/note-tuts?retryWrites=true&w=majority";
 
-//register view engine
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
+// register view engine
 app.set('view engine', 'ejs');
-
-
-// listen for requests
-
-//app.listen(3000);
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
-
-// mongoose & mongo tests
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-      title: 'new blog 2',
-      snippet: 'about my new blog',
-      body: 'more about my new blog'
-    })
-  
-    blog.save()
-      .then(result => {
-        res.send(result);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-//display all data in the database
-  app.get('/all-blog', (req, res) => {
-    Blog.find()
-      .then(result => {
-        res.send(result);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-
-//find by single id specific in the mongodb
-
-  app.get('/single-blog', (req, res) => { 
-    Blog.findById('656926bc4fbaf2914e2c348e')
-      .then(result => {
-        res.send(result);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-
-  app.get('/', (req, res) => {
-    res.redirect('/blogs');
-  });
-  
-  app.get('/about', (req, res) => {
-    res.render('about', { title: 'About' });
-  });
-  
-  // blog routes
-  app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-  });
-  
-  app.get('/blogs', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-      .then(result => {
-        res.render('index', { blogs: result, title: 'All blogs' });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-//...
-app.use((req, res, next) => {
-  console.log('new request made:');
-  console.log('host: ', req.hostname);
-  console.log('path: ', req.path);
-  console.log('method: ', req.method);
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('in the next middleware');
-  next();
-});
-
-app.use(morgan('dev'));
-
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
+
+// routes
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      ];
-
-    res.render('index', { title: 'Home', blogs});
-
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
-    res.render('about', { title: 'About' });
+  res.render('about', { title: 'About' });
 });
 
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Creat a New Blog' });
-});
+// blog routes
+app.use('/blogs', blogRoutes);
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).render('404', { title: '404' });
+  res.status(404).render('404', { title: '404' });
 });
